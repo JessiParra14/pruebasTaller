650160f702fe276ea9b508136007d275ee130932
daniellop31 <daniellop31@yahoo.es>
2020-11-18 19:19:16 -0500

    metodo solo letras

--- pruebasTaller/principal/principal.js
+++ pruebasTaller/principal/principal.js
@@ -5,3 +5,22 @@ exports.calculadora = function(operacion) {
     return Math.round(resultado, 2);
 
 }
+
+function soloLetras(e){
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = " áéíóúabcdefghijklmnñopqrstuvwxyzQWERTYUIOPASDFGHJKLÑZXCVBNM";
+    especiales = "8-37-39-46";
+    var i = 0
+    tecla_especial = false
+    for( var i = 0; i < especiales.length; i++){
+         if(key != especiales[i]){
+             tecla_especial = true;
+             break;
+         }
+     }
+
+     if(letras.indexOf(tecla)!=-1 && !tecla_especial){
+         return false;
+     }
+ }
